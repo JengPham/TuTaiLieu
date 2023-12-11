f1fbf67c3f3c9b7bd93961a044236b4b4ae79dcc
Pham Thanh Phong <jengpham@gmail.com>
2023-12-12 06:19:22 +0700

    .

---
+++ Libraries for Sensors/XY-MD20/modbus_crc.h
@@ -0,0 +1,14 @@
+/*
+ * modbus_crc.h
+ *
+ *  Created on: Dec 11, 2023
+ *      Author: jengp
+ */
+
+#ifndef INC_MODBUS_CRC_H_
+#define INC_MODBUS_CRC_H_
+
+#include "stdint.h"
+uint16_t crc16(uint8_t *buffer, uint16_t buffer_length);
+
+#endif /* INC_MODBUS_CRC_H_ */
